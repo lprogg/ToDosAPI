62544cb7ca91ffb788e039447a1fb4bea6fa1350
lprogg <lprogg>
2021-05-26 00:29:19 +0300

    Create

--- Project/Controllers/ToDosController.cs
+++ Project/Controllers/ToDosController.cs
@@ -1,3 +1,5 @@
+using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Project.Models;
 
@@ -10,5 +12,62 @@ namespace Project.Controllers
         private readonly ToDosContext _context;
 
         public ToDosController(ToDosContext context) => _context = context;
+
+        [HttpPost]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public ActionResult<ToDos> PostToDo(ToDos todo)
+        {
+            try
+            {
+                if (todo.Description == null)
+                {
+                    return BadRequest();
+                }
+
+                _context.ToDosItems.Add(todo);
+                _context.SaveChanges();
+
+                return CreatedAtAction("GetToDoItem", new ToDos {Id = todo.Id}, todo);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new
+                    {
+                        Message = e.InnerException != null
+                            ? $"{e.Message} {e.InnerException.Message}"
+                            : e.Message
+                    });
+            }
+        }
+
+        [HttpGet("{Id}")]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status304NotModified)]
+        public ActionResult<ToDos> GetToDoItem(int Id)
+        {
+            try
+            {
+                var todoItem = _context.ToDosItems.Find(Id);
+
+                if (todoItem == null)
+                {
+                    return NotFound();
+                }
+
+                return todoItem;
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new
+                    {
+                        Message = e.InnerException != null
+                            ? $"{e.Message} {e.InnerException.Message}"
+                            : e.Message
+                    });
+            }
+        }
     }
 }
